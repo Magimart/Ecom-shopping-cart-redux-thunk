6e521ca6693f2104e259d65f6bc5a5223b350148
Magimart <magmafelix2002@yahoo.com>
2021-12-28 11:55:19 +0100

    node14 downgrade

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const path = require('path');
 
+//npm WARN deprecated flatten@1.0.3: flatten is deprecated in favor of utility frameworks such as lodash.
+//npm WARN deprecated querystring@0.2.1: The querystring API is considered Legacy. new code should use the URLSearchParams API instead.
+//$ npm i -g npm
+//$ npm i --save lodash
+
 module.exports = {
   env : {    
 
@@ -10,9 +15,10 @@ module.exports = {
     STRIPE_WEBHOOK_SECRET: '',
   
  
-     MONGODB_URI: 'mongodb://localhost:27017/magimabioDb',
-     DATABASE_CLOUD:'mongodb+srv://',
-  
+    MONGODB_URI: 'mongodb://localhost:27017/magimabioDb',
+    DATABASE_CLOUD:'mongodb+srv://portfolioDb:frederick1610@final-projects-wbs-codi.pulb6.mongodb.net/bioDB?retryWrites=true&w=majority',
+ 
+
     NEXTAUTH_URL:'http://localhost:3000', // for production
    //  NEXTAUTH_URL_INTERNAL: devNextAuthUrl
 
@@ -29,10 +35,10 @@ module.exports = {
     FACEBOOK_SECRET:'',
     JWT_SECRET:'',
     
-  //____cloudinary strings
-  CLOUDINARY_NAME:'magimaart',
-  CLOUDINARY_API_KEY:'',
-  CLOUDINARY_API_SECRET:'',
+        //____cloudinary strings
+                CLOUDINARY_NAME:'magimaart',
+                CLOUDINARY_API_KEY:'399829674291311',
+                CLOUDINARY_API_SECRET:'4rfscNDKgSQQhPu_mxoFiNYsqoU',             
 
 }, 
 
